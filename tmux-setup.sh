28835019027bcbb23d98843b11c8326d1b0ffb6e
jacob zlogar <jacob.zlogar@gmail.com>
2019-04-23 16:13:46 -0400

    just CD

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-DIRECTORY=$1
-
-cd $DIRECTORY
+cd $1
 tmux new-window -n 'utils'
 tmux split-window -h
 tmux select-pane -t 0
@@ -10,8 +8,9 @@ tmux select-pane -t 1
 tmux send-keys "php artisan horizon" C-m
 tmux split-window -v
 tmux send-keys "npm run watch" C-m
+sleep 1
 tmux select-window -t 1
 tmux select-pane -t 0
 
-tmux send-keys "cd $DIRECTORY" C-m
+tmux send-keys "cd $1" C-m
 tmux send-keys "vim" C-m
